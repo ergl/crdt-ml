6af2d794c61da1be82756a43dbb11b5bf7cdc0d4
ergl <ergl@users.noreply.github.com>
2016-04-20 22:08:49 +0200

    Preped all mutable crdts with M_

--- src/crdt.ml
+++ src/crdt.ml
@@ -1,8 +1,8 @@
 module Mutable = struct
-  module IntVector = IntVector
-  module GCounter = GCounter
-  module PNCounter = PNCounter
-  module GSet = GSet
-  module ORSet = ORSet
-  module USet = USet
+  module IntVector = M_IntVector
+  module GCounter = M_GCounter
+  module PNCounter = M_PNCounter
+  module GSet = M_GSet
+  module ORSet = M_ORSet
+  module USet = M_USet
 end
