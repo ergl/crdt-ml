b9c3f51eb839634fa091efbe316ede7bf327560a
ergl <ergl@users.noreply.github.com>
2016-04-20 16:24:20 +0200

    Fix USet.remove, ignore removes if element is not a member

--- src/USet.ml
+++ src/USet.ml
@@ -30,13 +30,15 @@ module Make (O : OrderedType) = struct
     let (a, r) = !s in
     s := (ISet.add el a, r)
 
-  let remove s el =
-    let (a, r) = !s in
-    s := (a, ISet.add el r)
-
   let lookup s el =
     let (a, r) = !s in
     ISet.mem el @@ ISet.diff a r
+
+  let remove s el =
+    if lookup s el then begin
+      let (a, r) = !s in
+      s := (a, ISet.add el r)
+    end
 
   let merge s s' =
     let (a, r) = !s and
